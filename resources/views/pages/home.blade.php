553806c73dd98e93860ca5cde5ebb69e57ec371a
Mudassir Maqbool <mudassirmaqboolofficial@gmail.com>
2020-01-29 03:15:24 +0500

    adk

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,11 +9,11 @@
         <section class="hiw">
             <div class="fsec">
                 <div class="container">
-                    <div class="heading">How It Works</div>
+                    {{-- <div class="heading">How It Works</div> --}}
                     <div class="row">
                         <div class="col-md-7">
                             <div class="data">
-                                <div class="step"><span>Step 1</span></div>
+                                {{-- <div class="step"><span>Step 1</span></div> --}}
                                 <div class="title">Register & Setup Profile</div>
                                 <div class="text">You can register as a student or a tutor. Fill the registeration form and setup your profile.</div>
                             </div>
@@ -37,8 +37,8 @@
                         </div>
                         <div class="col-md-7">
                             <div class="data">
-                                <div class="step">Step 2.1</div>
-                                <div class="title">Search & Find Tutors Around You</div>
+                                {{-- <div class="step">Step 2.1</div> --}}
+                                <div class="title">Search Tutors</div>
                                 <div class="text">As a student you can search for tutors around you in matter of few clicks.</div>
                             </div>
                         </div>
@@ -51,7 +51,7 @@
                     <div class="row">
                         <div class="col-md-7">
                             <div class="data">
-                                <div class="step">Step 2.2</div>
+                                {{-- <div class="step">Step 2.2</div> --}}
                                 <div class="title">Post an Ad</div>
                                 <div class="text">Students can also post ad that will be visible to tutors within selected area.</div>
                             </div>
@@ -74,7 +74,7 @@
                         </div>
                         <div class="col-md-7">
                             <div class="data">
-                                <div class="step">Step 3</div>
+                                {{-- <div class="step">Step 3</div> --}}
                                 <div class="title">Contact Tutor</div>
                                 <div class="text">Tutors-Hub offer real-time chat service for free. Students and tutors can communicate with eachother on the website.</div>
                             </div>
@@ -88,7 +88,7 @@
                     <div class="row">
                         <div class="col-md-7">
                             <div class="data">
-                                <div class="step">Step 4</div>
+                                {{-- <div class="step">Step 4</div> --}}
                                 <div class="title">Book a Session Online</div>
                                 <div class="text">Student can book sessions with tutors on the website by selecting a plan offered by tutor and number of hours. Student will pay the booking amount online.</div>
                             </div>
@@ -111,7 +111,7 @@
                         </div>
                         <div class="col-md-7">
                             <div class="data">
-                                <div class="step">Step 5</div>
+                                {{-- <div class="step">Step 5</div> --}}
                                 <div class="title">Rate & Leave Feedback</div>
                                 <div class="text">After a session, student and tutor can rate and leave a revew for eachother. All reviews and feedbacks will be visible on user's profile</div>
                             </div>
