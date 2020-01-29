afabc43d0c16b4deb75dd43e5fcfad854cc227a0
Mudassir Maqbool <mudassirmaqboolofficial@gmail.com>
2020-01-29 20:51:04 +0500

    asddl

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,14 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon">
     <title>@yield('title')</title>
+    @yield('meta')
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/animate.css')}}">
     <link href="{{asset('css/fontawesome-all.min.css')}}"  rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
-    
-</script>
+
+
     @yield('styles')
 </head>
 <body>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,7 +2,72 @@
 @section('title')
     Home
 @endsection
+@section('meta')
+    <style>
+      .pre-loader{
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: #fbfbfb;
+        z-index: 111;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+      .hidden{
+          display: none;
+      }
+      .lds-ring {
+        display: inline-block;
+        position: relative;
+        width: 80px;
+        height: 80px;
+        }
+        .lds-ring div {
+        box-sizing: border-box;
+        display: block;
+        position: absolute;
+        width: 64px;
+        height: 64px;
+        margin: 8px;
+        border: 8px solid rgb(39, 179, 51);
+        border-radius: 50%;
+        animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+        border-color: rgb(181, 255, 134) transparent transparent transparent;
+        }
+        .lds-ring div:nth-child(1) {
+        animation-delay: -0.45s;
+        }
+        .lds-ring div:nth-child(2) {
+        animation-delay: -0.3s;
+        }
+        .lds-ring div:nth-child(3) {
+        animation-delay: -0.15s;
+        }
+        @keyframes lds-ring {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+        }
+    </style>
+    <script>
+        window.addEventListener("load", function()
+        {
+            const loader = document.querySelector(".pre-loader");
+            loader.className += " hidden";
+        })
+    </script>
+@endsection
 @section('content')
+    <div class="pre-loader">
+            <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+
+    </div>
     <div id="app"  class="homepage">
         <register-form url="{{route('home')}}" ></register-form>
         
