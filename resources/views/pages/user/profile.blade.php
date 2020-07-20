c36e2437e6b12a9a1251c3ca3a1a2a113f9b99ae
tricasol <tricasolutions@gmail.com>
2020-07-20 16:24:56 +0500

    NEw Changes

--- resources/views/pages/user/profile.blade.php
+++ resources/views/pages/user/profile.blade.php
@@ -3,6 +3,14 @@
     {{auth()->user()->name}}
 @endsection
 @section('content')
+    <style>
+        .book-section {
+            display: block;
+            text-align: center;
+            max-width: 500px;
+            margin: auto;
+        }
+        </style>
     @php
         if($user->type == 'teacher')
         {
@@ -14,5 +22,5 @@
     <div id="app">
     <profile url="{{route('home')}}" :likes="{{$likes}}" :user="{{$user}}" :authuser="{{auth()->user()}}" rating="{{$user->profile->completedSessions->average($type)}}"></profile>
     </div>
-    
+
 @endsection
