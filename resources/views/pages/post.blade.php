b3e8e92ef90ee3cfa6d5ec6edc0590e9fe351004
Malik Zeeshan <abdullahsiyal@gmail.com>
2020-09-17 00:46:58 +0500

    17-09-2020

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -1,17 +1,29 @@
+<style>
+    
+    /* Header/Logo Title */
+    .header {
+      padding: 150px;
+      text-align: center;
+      /* background: #1abc9c; */
+      color: white;
+      font-size: 30px;
+    }
+    
+    /* Page Content */
+    /* .content {padding:20px;} */
+    </style>
 @extends('layouts.master')
 @section('title')
 Blog | Labs-Hub
 @endsection
 @section('content')
-    <div class="post-page">
-        <div class="post-header" style="background: linear-gradient( -45deg, #eb8832d6, #000000d9),  url({{asset('storage/blog') . "/" . $post->image}})">
-            <h1 class="title">{{$post->title}}</h1>
-            <div class="date">{{Carbon\Carbon::parse($post->created_at)->format('d M, Y')}}</div>
-        </div>
-        <div class="post-body">
-            <div class="container">
-               <p> {!! $post->description !!}</p> 
-            </div>
+    <div class="header" style="background: linear-gradient( -45deg, #eb8832d6, #000000d9),  url({{asset('storage/blog') . "/" . $post->image}})">
+        <h1>{{$post->title}}</h1>
+        <p>{{Carbon\Carbon::parse($post->created_at)->format('d M, Y')}}</p>
+    </div>
+    <div class="post-body">
+        <div class="container">
+            <p> {!! $post->description !!}</p> 
         </div>
     </div>
 @endsection
